19d5b3df7d79662ba5ab7aa22f906e6f11bc5683
xpingc <xiaoping@outlook.com>
2016-02-06 20:31:57 -0800

    Add express-handlebars in the sample
    This is the code for chapter 3, where we add express-handlebars as the
    template.

--- meadowlark.js
+++ meadowlark.js
@@ -2,29 +2,31 @@ var express = require('express');
 
 var app = express();
 
+// set up handlebars view engine
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
 app.set('port', 3000);
 
 app.get('/', function(req, res) {
-	res.type('text/plain');
-	res.send('Meadowlark Travel');
+	res.render('home');
 });
 
 app.get('/about', function(req, res) {
-	res.type('text/plain');
-	res.send('About Meadowlark Travel');
+	res.render('about');
 });
 
 app.use(function(req, res) {
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
 });
 
 app.use(function(err, req, res, next) {
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.send('500');
 });
 
 app.listen(app.get('port'), function() {
